a1bbd7189e35d9d037e9173e15698df33953f355
ferencDobi <ferencdbi@gmail.com>
2018-08-27 15:02:43 +0200

    enemy spawning beta

--- src/com/codecool/snake/GameLoop.java
+++ src/com/codecool/snake/GameLoop.java
@@ -2,13 +2,22 @@ package com.codecool.snake;
 
 import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.entities.Animatable;
+import com.codecool.snake.entities.enemies.SimpleEnemy;
 import javafx.animation.AnimationTimer;
+import javafx.scene.layout.Pane;
 
 public class GameLoop extends AnimationTimer {
+    private Pane game;
+
+    GameLoop(Pane pane) {
+        super();
+        game = pane;
+    }
 
     // This gets called every 1/60 seconds
     @Override
     public void handle(long now) {
+        if ((int) (now / 16666666.6667) % 3600 == 0) new SimpleEnemy(game);
         for (GameEntity gameObject : Globals.gameObjects) {
             if (gameObject instanceof Animatable) {
                 Animatable animObject = (Animatable)gameObject;
